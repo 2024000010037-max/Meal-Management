7e48983eaa48ac4cf744f253f313d1f5619b9945
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-11 21:01:34 +0600

    Update approve_deposit.php
    Config SMTP and connect Database

--- manager/approve_deposit.php
+++ manager/approve_deposit.php
@@ -4,5 +4,15 @@ if (!isset($_SESSION['user_id']) || !in_array($_SESSION['role'], ['manager', 'ad
     header("Location: ../index.php");
     exit;
 }
+    include "../config/database.php";
+require '../sms/Exception.php';
+require '../sms/PHPMailer.php';
+require '../sms/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$pdo = (new Database())->connect();
+
 
 ?>
