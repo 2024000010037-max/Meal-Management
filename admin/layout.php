a79b98d0a204577b276a44ef1356b38f4a377bc0
MD Towfiq Omar <1towfiq12@gmail.com>
2026-01-11 12:56:28 +0600

    Update layout.php

--- admin/layout.php
+++ admin/layout.php
@@ -45,5 +45,9 @@
             <?= isset($content) ? $content : '' ?>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+
+    </script>
 </body>
 </html>
