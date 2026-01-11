7f8da0cff161571f10253a4e351625f85d2942ff
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-11 17:40:48 +0600

    Update account.php
    connect database

--- manager/account.php
+++ manager/account.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id']) || !in_array($_SESSION['role'], ['manager', 'admin'])) {
+    header("Location: ../index.php");
+    exit;
+}
 
+include "../config/database.php";
+$pdo = (new Database())->connect();
+$user_id = $_SESSION['user_id'];
+
+?>
