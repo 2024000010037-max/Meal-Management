0275a013249d73f52634a8abe80814491d62d761
MD Towfiq Omar <1towfiq12@gmail.com>
2026-01-11 13:30:24 +0600

    Update create_user.php
    conection database

--- admin/create_user.php
+++ admin/create_user.php
@@ -4,7 +4,12 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
     header("Location: ../index.php");
     exit;
 }
+include "../config/database.php";
 
+$pdo = (new Database())->connect();
+
+$msg = "";
+$editUser = null;
 
 
 
