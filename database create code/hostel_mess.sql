12e65063c099e548648aa3c51c0a4e0184650539
MD Towfiq Omar <1towfiq12@gmail.com>
2026-01-11 13:19:10 +0600

    Update and rename hostel_mess to hostel_mess.sql
    all user create user role set active in active all set this file

---
+++ database create code/hostel_mess.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(100) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('admin','manager','user') DEFAULT NULL,
+  `status` tinyint(4) DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `nid_photo` varchar(255) DEFAULT NULL,
+  `is_auto_meal` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
