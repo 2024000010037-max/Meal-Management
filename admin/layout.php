e2ef37366c89f2fca3464f7ecb6bb5366327ac86
MD Towfiq Omar <1towfiq12@gmail.com>
2026-01-11 12:56:43 +0600

    Update layout.php

--- admin/layout.php
+++ admin/layout.php
@@ -47,7 +47,11 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-
+    // Sidebar Toggle Logic
+        document.getElementById('menu-toggle').addEventListener('click', function() {
+            document.body.classList.toggle('toggled');
+        });
+        
     </script>
 </body>
 </html>
