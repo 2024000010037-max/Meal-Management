97e9e1275b5d26f6781caa32d92cbbedff515cbd
MD Towfiq Omar <1towfiq12@gmail.com>
2026-01-11 13:09:39 +0600

    Update database.php
    error checking and db conection

--- config/database.php
+++ config/database.php
@@ -4,6 +4,14 @@ class Database {
     private $db = "hostel_mess";
     private $user = "root";
     private $pass = "Towfiq7020@";
-  
+    
+   public function connect() {
+        try {
+            return new PDO(
+                "mysql:host={$this->host};dbname={$this->db}",
+                $this->user,
+                $this->pass,
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
 }
 </php>
