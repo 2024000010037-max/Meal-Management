6ad14f233bdd97e6cc30e94121833525e3149d18
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-11 20:16:45 +0600

    Update deposit.php
    SMTP added for otp

--- manager/deposit.php
+++ manager/deposit.php
@@ -8,4 +8,17 @@ if (!isset($_SESSION['user_id']) || !in_array($_SESSION['role'], ['manager', 'ad
 include "../config/database.php";
 $pdo = (new Database())->connect();
 
+require '../sms/Exception.php';
+require '../sms/PHPMailer.php';
+require '../sms/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$msg = "";
+$selected_month = $_GET['month'] ?? date('Y-m');
+$manager_id = $_SESSION['user_id'];
+
+
+
 ?>
