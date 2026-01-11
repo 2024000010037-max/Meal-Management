be72cda980d04e2e548bb04da0f158b1ee8aa8ee
MD Towfiq Omar <1towfiq12@gmail.com>
2026-01-11 13:31:31 +0600

    Update create_user.php

--- admin/create_user.php
+++ admin/create_user.php
@@ -11,6 +11,17 @@ $pdo = (new Database())->connect();
 $msg = "";
 $editUser = null;
 
+// Helper Function for File Upload
+function uploadFile($file, $dir, $prefix) {
+    if ($file['error'] === UPLOAD_ERR_OK) {
+        $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+        $filename = $prefix . '_' . time() . '_' . uniqid() . '.' . $ext;
+        if (move_uploaded_file($file['tmp_name'], $dir . $filename)) {
+            return $filename;
+        }
+    }
+    return null;
+}
 
 
 </php>  
