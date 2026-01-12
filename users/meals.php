cfec5a31829f0371c8af46b95fe4085be8c0ce53
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 21:26:37 +0600

    Update meals.php

--- users/meals.php
+++ users/meals.php
@@ -12,5 +12,12 @@ date_default_timezone_set('Asia/Dhaka');
 $userId = $_SESSION['user_id'];
 $selected_date = $_GET['date'] ?? date('Y-m-d');
 $msg = "";
+// --- TIME CONSTRAINTS LOGIC (MOVED UP) ---
+$now = new DateTime();
+$target = new DateTime($selected_date);
+$today_str = $now->format('Y-m-d');
+$target_str = $target->format('Y-m-d');
+$hour = (int)$now->format('H');
+
 
 
