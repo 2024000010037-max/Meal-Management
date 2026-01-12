950203b0127957c9ead8ec995afae77c8fef8ab8
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 19:17:58 +0600

    Update send_invoice.php

--- manager/send_invoice.php
+++ manager/send_invoice.php
@@ -13,6 +13,13 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 $pdo = (new Database())->connect();
+$user_id = isset($_GET['user_id']) ? intval($_GET['user_id']) : 0;
+$selected_month = $_GET['month'] ?? date('Y-m');
+
+if (!$user_id) {
+    die("Invalid User ID");
+}
+
 
 
 ?>
