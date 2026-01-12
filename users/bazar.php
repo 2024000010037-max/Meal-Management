827663a4e54ec41316c67795b6628f77fdbd98d0
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 21:53:40 +0600

    Update bazar.php

--- users/bazar.php
+++ users/bazar.php
@@ -4,4 +4,10 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'user') {
     header("Location: ../index.php");
     exit;
 }
+include "../config/database.php";
+$pdo = (new Database())->connect();
+
+$userId = $_SESSION['user_id'];
+$selected_month = $_GET['month'] ?? date('Y-m');
+$msg = "";
 
