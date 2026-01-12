eddef550ab69d2cef4ba9d85d69cdf5e1492dd8b
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 07:05:11 +0600

    Update index.php

--- users/index.php
+++ users/index.php
@@ -25,3 +25,4 @@ if ( ! $is_elementor_theme_exist || ! elementor_theme_do_location( 'archive' ) )
 get_template_part( 'template-parts/search' );
 	}
 } else {
+if ( ! $is_elementor_theme_exist || ! elementor_theme_do_location( 'single' ) ) {
