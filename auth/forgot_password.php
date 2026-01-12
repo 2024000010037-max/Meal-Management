8654369699c4f58678d1a36c4055e91361662752
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 20:34:44 +0600

    Update forgot_password.php
    floating food icon in background

--- auth/forgot_password.php
+++ auth/forgot_password.php
@@ -10,3 +10,10 @@ session_start();
   
 </head>
 <body class="d-flex align-items-center justify-content-center">
+<!-- Floating Food Icons -->
+  <div class="food-icon" style="top: 15%; left: 10%; animation-delay: 0s;">🍔</div>
+  <div class="food-icon" style="top: 25%; right: 15%; animation-delay: 1s;">🍕</div>
+  <div class="food-icon" style="bottom: 20%; left: 15%; animation-delay: 2s;">🍚</div>
+  <div class="food-icon" style="bottom: 15%; right: 10%; animation-delay: 3s;">🍗</div>
+  <div class="food-icon" style="top: 50%; left: 5%; animation-delay: 1.5s;">🥗</div>
+  <div class="food-icon" style="top: 10%; left: 50%; animation-delay: 0.5s;">🍳</div>
