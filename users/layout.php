7eb5d6356ce69dd206e0309701c3f63e38d2d1da
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 07:53:30 +0600

    Update layout.php

--- users/layout.php
+++ users/layout.php
@@ -123,3 +123,4 @@ toggle="dropdown" aria-expanded="false">
  $nav_img = "https://ui-avatars.com/api/?name=" . urlencode($_SESSION['full_name'] ?? 'User') . "&background=0D6EFD&color=fff";
 if(isset($_SESSION['user_id']) && isset($pdo)) {
  $stmt_nav = $pdo->prepare("SELECT photo FROM users WHERE id = ?");
+ $stmt_nav->execute([$_SESSION['user_id']]);
