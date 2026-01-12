3a1c26f150c7a63af5f81a4a5e1ba6ab0c39a60c
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 19:24:41 +0600

    Update send_invoice.php

--- manager/send_invoice.php
+++ manager/send_invoice.php
@@ -127,8 +127,8 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'remarkhb.herlanit@gmail.com'; // Using credentials from context
-    $mail->Password   = 'mutq ddwp qkyu hzgo';
+    $mail->Username   = 'demo@gmail.com'; // Using credentials from context
+    $mail->Password   = 'xxxxxxxx';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port       = 587;
 
