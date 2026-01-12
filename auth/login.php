01388eca6c2f51e63d3cfdb39682fe75ab3cb794
MD Towfiq Omar <1towfiq12@gmail.com>
2026-01-12 13:00:10 +0600

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -69,3 +69,15 @@ body {
         }
 </style>
 </head>
+<body class="d-flex align-items-center justify-content-center">
+
+    <!-- Floating Food Icons -->
+    <div class="food-icon" style="top: 15%; left: 10%; animation-delay: 0s;">🍔</div>
+    <div class="food-icon" style="top: 25%; right: 15%; animation-delay: 1s;">🍕</div>
+    <div class="food-icon" style="bottom: 20%; left: 15%; animation-delay: 2s;">🍚</div>
+    <div class="food-icon" style="bottom: 15%; right: 10%; animation-delay: 3s;">🍗</div>
+    <div class="food-icon" style="top: 50%; left: 5%; animation-delay: 1.5s;">🥗</div>
+    <div class="food-icon" style="top: 10%; left: 50%; animation-delay: 0.5s;">🍳</div>
+
+
+</body>
