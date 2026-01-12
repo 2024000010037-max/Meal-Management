21870db570bee57b376bbfa22bfd1c1d8eef998d
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 07:58:49 +0600

    Update layout.php

--- users/layout.php
+++ users/layout.php
@@ -151,3 +151,4 @@ Logout</a></li>
     </div>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
+document.getElementById('menu-toggle').addEventListener('click', function() {
