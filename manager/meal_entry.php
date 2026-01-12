039977a0f08f2156d1dda5e85dc65a2a5badaf9d
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 11:24:14 +0600

    Update meal_entry.php
    Auto meal entry

--- manager/meal_entry.php
+++ manager/meal_entry.php
@@ -15,6 +15,11 @@ $selected_date = $_GET['date'] ?? date('Y-m-d');
 
 if (isset($_GET['toggle_auto']) && isset($_GET['uid'])) {
     $uid = intval($_GET['uid']);
+       $pdo->prepare("UPDATE users SET is_auto_meal = NOT is_auto_meal WHERE id = ?")->execute([$uid]);
+    header("Location: meal_entry.php?date=" . $selected_date);
+    exit;
+}
+
 
 
 ?>
