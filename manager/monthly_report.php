fa61e59122832fc17e0dd15aac98459d58c94fc6
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 18:51:05 +0600

    Update monthly_report.php
    Total mess deposit

--- manager/monthly_report.php
+++ manager/monthly_report.php
@@ -20,6 +20,11 @@ $stmt = $pdo->prepare("SELECT SUM(amount) FROM bazar WHERE status = 'approved' A
 $stmt->execute([$selected_month]);
 $total_mess_bazar = $stmt->fetchColumn() ?: 0;
 
+// Total Mess Deposit
+$stmt = $pdo->prepare("SELECT SUM(amount) FROM deposits WHERE status = 'approved' AND DATE_FORMAT(deposit_date, '%Y-%m') = ?");
+$stmt->execute([$selected_month]);
+$total_mess_deposit = $stmt->fetchColumn() ?: 0;
+
 
 
 ?>
