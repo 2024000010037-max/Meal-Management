897cc3d4b46eb75be8f305b1579a6e2e875bee93
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 08:16:16 +0600

    Update account.php

--- users/account.php
+++ users/account.php
@@ -46,3 +46,5 @@ div class="mb-3 border-bottom pb-2">
                     <label class="small text-muted d-block mb-1">Phone Number</label>
                     <span class="fw-bold text-dark fs-5"><?= htmlspecialchars($user['phone'] ?? 'Not Provided') ?></span>
                 </div>
+<div class="mb-3">
+                    <label class="small text-muted d-block mb-1">Account Status</label>
