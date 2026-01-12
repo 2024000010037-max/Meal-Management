245a8e7adfe1fd480c2e4d26dc9f3e4de455c062
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 11:01:12 +0600

    Update meal_entry.php
    Database Connected with meal entry

--- manager/meal_entry.php
+++ manager/meal_entry.php
@@ -1,1 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'manager') {
+    header("Location: ../index.php");
+    exit;
+}
 
+include "../config/database.php";
+$pdo = (new Database())->connect();
+
+?>
