37023f07b8c774575ba05b653626fff4fb0bd38d
MD Towfiq Omar <1towfiq12@gmail.com>
2026-01-12 12:07:34 +0600

    Create approve_deposit.php

---
+++ admin/approve_deposit.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id']) || !in_array($_SESSION['role'], ['manager', 'admin'])) {
+    header("Location: ../index.php");
+    exit;
+}
+
+include "../config/database.php";
+require '../sms/Exception.php';
+require '../sms/PHPMailer.php';
+require '../sms/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
