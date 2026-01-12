904a5281d8021c1e627d451e16eb0403ae4c91c2
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 07:58:23 +0600

    Update layout.php

--- users/layout.php
+++ users/layout.php
@@ -149,3 +149,5 @@ Logout</a></li>
         <?= isset($content) ? $content : '' ?>
         </div>
     </div>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
