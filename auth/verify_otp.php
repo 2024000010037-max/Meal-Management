cbdd762dd03ac7e5337fb5f417454779da4b781f
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 20:27:11 +0600

    Update verify_otp.php

--- auth/verify_otp.php
+++ auth/verify_otp.php
@@ -3,6 +3,10 @@ session_start();
 $user_otp = $_POST['otp'] ?? '';
 $session_otp = $_SESSION['otp'] ?? '';
 $otp_time = $_SESSION['otp_time'] ?? 0;
+if (!$session_otp) {
+    echo "Session expired. Please request a new OTP.";
+    exit;
+}
 
 
 ?>
