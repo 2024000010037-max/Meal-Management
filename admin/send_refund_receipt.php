760944e70b03bd196d5768f50bff9b1ff2ad8175
MD Towfiq Omar <1towfiq12@gmail.com>
2026-01-12 12:11:06 +0600

    Create send_refund_receipt.php

---
+++ admin/send_refund_receipt.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id']) || !in_array($_SESSION['role'], ['manager', 'admin'])) {
+    header("Location: ../index.php");
+    exit;
+}
+
+include "../config/database.php";
+require '../sms/Exception.php';
+require '../sms/PHPMailer.php';
+require '../sms/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$pdo = (new Database())->connect();
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+$month = $_GET['month'] ?? date('Y-m');
+
+if (!$id) {
+    header("Location: deposit.php?month=$month");
+    exit;
+}
