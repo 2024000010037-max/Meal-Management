64aca55411bc4ba59d16f3fe6ab72ccd48d96769
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 22:33:06 +0600

    Update dashboard.php

--- users/dashboard.php
+++ users/dashboard.php
@@ -230,6 +230,10 @@ ob_start();
             <span class="fw-bold">My Deposits</span>
         </a>
     </div>
+ <div class="col-6 col-md-3 mb-3">
+        <a href="meals.php" class="btn btn-outline-secondary w-100 p-3 h-100 d-flex flex-column align-items-center justify-content-center gap-2 rounded-4">
+            <i class="bi bi-clock-history fs-3"></i>
+            <span class="fw-bold">History</span>
 
 
 
