000ffff871d0d314bda015f62ab6efbdd04b3c12
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 07:45:27 +0600

    Update layout.php

--- users/layout.php
+++ users/layout.php
@@ -89,4 +89,11 @@ body.toggled .main-content {
    body.toggled .sidebar {
                 left: 0;
             }
-       
+  body.toggled::before {
+                content: '';
+                position: fixed; top: 0; left: 0; right: 0; bottom: 0;
+                background: rgba(0,0,0,0.5);
+                z-index: 999;
+            }
+        }
+     
