d2463579d8c6fb19f39f000a4d840b83308a3247
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 14:07:10 +0600

    Update meal_history.php
    time date configure

--- manager/meal_history.php
+++ manager/meal_history.php
@@ -6,6 +6,10 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'manager') {
 }
     include "../config/database.php";
 $pdo = (new Database())->connect();
+$start_date = $_GET['start_date'] ?? date('Y-m-01');
+$end_date   = $_GET['end_date'] ?? date('Y-m-t');
+$search     = $_GET['search'] ?? '';
+
 
 
 
