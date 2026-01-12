aa5025e36375f3fb3000894a5c952420648a1639
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 07:18:58 +0600

    Update layout.php

--- users/layout.php
+++ users/layout.php
@@ -7,3 +7,4 @@
 <!-- Bootstrap 5 -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <!-- Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
