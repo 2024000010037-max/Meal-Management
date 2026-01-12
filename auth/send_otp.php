7de1270d624d8eb1f883e6b7231192d33c24767e
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 20:00:34 +0600

    Update send_otp.php
    include php mailer

--- auth/send_otp.php
+++ auth/send_otp.php
@@ -2,5 +2,14 @@
 session_start();
 include "../config/database.php";
 
+// Include PHPMailer from the 'sms' folder as requested
+require '../sms/Exception.php';
+require '../sms/PHPMailer.php';
+require '../sms/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
 
 ?>
