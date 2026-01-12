6d8424388199900515659ee2caded99d9655ec2b
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-12 20:44:33 +0600

    Update forgot_password.php
    include bootstrap

--- auth/forgot_password.php
+++ auth/forgot_password.php
@@ -174,5 +174,5 @@ function verifyOTP() {
         .finally(() => toggleButtonLoading('change-pass-btn', false));
     }
   </script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
