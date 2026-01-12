f4ebeed0aafee2097d9aff5512deaad600e16eba
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 07:14:57 +0600

    Update layout.php

--- users/layout.php
+++ users/layout.php
@@ -4,3 +4,4 @@
  <meta charset="UTF-8">
 <title><?= isset($pageTitle) ? $pageTitle : 'User Panel' ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Bootstrap 5 -->
