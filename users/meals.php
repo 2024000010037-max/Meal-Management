b20a2b253361b656377534153ad74ed3c6b11f80
Shakibul Hassan Shanto <2024000010079@seu.edu.bd>
2026-01-12 21:25:48 +0600

    Update meals.php

--- users/meals.php
+++ users/meals.php
@@ -4,4 +4,10 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'user') {
     header("Location: ../index.php");
     exit;
 }
+include "../config/database.php";
+$pdo = (new Database())->connect();
+
+// Set Timezone
+date_default_timezone_set('Asia/Dhaka');
+
 
