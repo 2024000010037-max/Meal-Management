0de91bc0d63411618a53980fe06661d9b649e3cb
Md Abjal Hossen <2024000010037@seu.edu.bd>
2026-01-13 19:02:10 +0600

    Update reset_password.php
    otp auth

--- auth/reset_password.php
+++ auth/reset_password.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
 include "../config/database.php";
+if (!isset($_SESSION['otp_verified']) || $_SESSION['otp_verified'] !== true) {
+    echo "Unauthorized access.";
+    exit;
+}
 
 ?>
